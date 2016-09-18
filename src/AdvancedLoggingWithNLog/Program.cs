6e04516f3bf3ba8fb71f9eca9830a070b79c9400
Kjellski <otto.kjell@gmail.com>
2016-09-18 14:41:56 +0200

    initial commit

--- src/AdvancedLoggingWithNLog/Program.cs
+++ src/AdvancedLoggingWithNLog/Program.cs
@@ -9,6 +9,7 @@ namespace AdvancedLoggingWithNLog
     {
         public static void Main(string[] args)
         {
+
         }
     }
 }
---
+++ src/GettingStarted/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NLog;
+using NLog.Config;
+using NLog.Targets;
+
+namespace GettingStarted
+{
+    public class Program
+    {
+        private static NLog.Logger Log = NLog.LogManager.GetCurrentClassLogger();
+
+        public static void Main(string[] args)
+        {
+            // Step 1. Create configuration object 
+            var config = new LoggingConfiguration();
+
+            // Step 2. Create targets and add them to the configuration 
+            var consoleTarget = new ColoredConsoleTarget();
+            config.AddTarget("console", consoleTarget);
+            
+            // Step 3. Set target properties 
+            consoleTarget.Layout = @"${date:format=HH\:mm\:ss} ${logger} ${message}";
+
+            // Step 4. Define rules
+            var rule1 = new LoggingRule("*", LogLevel.Debug, consoleTarget);
+            config.LoggingRules.Add(rule1);
+
+            // Step 5. Activate the configuration
+            LogManager.Configuration = config;
+
+            Log.Trace("Sample trace message");
+            Log.Debug("Sample debug message");
+            Log.Info("Sample informational message");
+            Log.Warn("Sample warning message");
+            Log.Error("Sample error message");
+            Log.Fatal("Sample fatal error message");
+
+            // alternatively you can call the Log() method 
+            // and pass log level as the parameter.
+            Log.Log(NLog.LogLevel.Info, "Sample informational message");
+
+            Console.WriteLine("Press <Enter> to exit...");
+            Console.ReadLine();
+        }
+    }
+}
