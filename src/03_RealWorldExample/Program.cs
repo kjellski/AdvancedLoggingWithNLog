f361a34ca93bc7f134f1d39e6d0b0ab536a59a3a
Kjellski <otto.kjell@gmail.com>
2016-09-20 23:25:31 +0200

    - fixed references for new projects - added basic config for NLog - added real world config for NLog

--- src/03_RealWorldExample/Program.cs
+++ src/03_RealWorldExample/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using NLog;
+using NLog.Config;
+using NLog.Targets;
 
-namespace ConfigFilesSetup
+namespace RealWorldExample
 {
     public class Program
     {
@@ -9,6 +11,23 @@ namespace ConfigFilesSetup
 
         public static void Main(string[] args)
         {
+            // Step 1. Create configuration object
+            var config = new LoggingConfiguration();
+
+            // Step 2. Create targets and add them to the configuration
+            var consoleTarget = new ColoredConsoleTarget();
+            config.AddTarget("console", consoleTarget);
+
+            // Step 3. Set target properties
+            consoleTarget.Layout = @"${date:format=HH\:mm\:ss} ${logger} ${message}";
+
+            // Step 4. Define rules
+            var rule1 = new LoggingRule("*", LogLevel.Debug, consoleTarget);
+            config.LoggingRules.Add(rule1);
+
+            // Step 5. Activate the configuration
+            LogManager.Configuration = config;
+
             Log.Trace("Sample trace message");
             Log.Debug("Sample debug message");
             Log.Info("Sample informational message");
